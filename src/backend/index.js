7f8dc4da0308d6d89d1b079102edbb9b31a25ac7
HMQ <hmquan191@gmail.com>
2024-11-20 23:21:44 +0700

    setup folders

---
+++ src/backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv').config()
+const path = require('path');
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use('/', require('./src/routes/authRoutes'));
+app.use(express.static(path.join(__dirname, 'src/public')));
+
+app.get("/", (req,res) => {
+    res.send("<h1>Home page</h1>")
+});
+
+const port = 8000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+
+
+
+
--- src/backend/src/routes/authRoutes.js
+++ src/backend/src/routes/authRoutes.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authController');
+const authController = require('../controller/authController');
 
-// route cho đăng ký (signup)
-router.post('/signup', authController.signup);
 
-// route cho đăng nhập (login)
-router.post('/login', authController.login);
+const cors = require('cors')
+const {test} = require('../controller/authController')
+
+// middleware
+
+router.use(
+    cors({
+        credentials: true,
+        origin: 'http://localhost:5173'
+    })
+)
+
+router.get('/', test)
+
+
+
+// router.post('/signup', authController.signup);
+
+
+// router.post('/login', authController.login);
 
 module.exports = router;
