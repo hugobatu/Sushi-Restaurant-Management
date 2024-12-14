dbcbc38ee10bdd7af806a86ffda713d48968bad6
KhaiSV <22127174@student.hcmus.edu.vn>
2024-12-14 16:00:16 +0700

    fixing staffs

--- src/frontend/app/a/staffs/page.tsx
+++ src/frontend/app/a/staffs/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { AdminHeader } from "@/components/Admin/admin-header"
 import { SideNav } from "@/components/Admin/side-nav"
 import { Button } from "@/components/ui/button"
+import { ChevronDown, ArrowUpDown, Plus, Search } from 'lucide-react';
 
 const StaffsPage = () => {
   return (
@@ -10,31 +11,27 @@ const StaffsPage = () => {
       <SideNav />
       <div className="ml-64 mt-4">
         <div className="w-full text-sm text-left rtl:text-right text-black">
-          <div className="flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4">
-            <div>
-                <button id="dropdownRadioButton" data-dropdown-toggle="dropdownRadio" className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
-                    Choose staff status
-                    <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-                    </svg>
-                </button>
-                <div id="dropdownRadio" className="z-10 hidden w-48 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" data-popper-reference-hidden="" data-popper-escaped="" data-popper-placement="top" style={{position: 'absolute', inset: 'auto auto 0px 0px', margin: '0px', transform: 'translate3d(522.5px, 3847.5px, 0px)',}}>
-                    <ul className="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownRadioButton">
-                        <li>
-                            <div className="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                                <input checked id="filter-radio-example-3" type="radio" value="" name="filter-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
-                                <label htmlFor="filter-radio-example-3" className="w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">All status</label>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <label htmlFor="table-search" className="sr-only">Search</label>
-            <div className="relative right-10 rtl:right-auto">
-              <div className="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
-                <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+          <div className="relative flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4 h-14">
+            <div className="absolute left-0 top-0">
+              <button id="dropdownRadioButton" data-dropdown-toggle="dropdownRadio" className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
+                  Choose staff status<ChevronDown className="w-4 h-4 ms-1.5" />
+              </button>
+              <div id="dropdownRadio" className="z-10 hidden w-48 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" data-popper-reference-hidden="" data-popper-escaped="" data-popper-placement="top" style={{position: 'absolute', inset: 'auto auto 0px 0px', margin: '0px', transform: 'translate3d(522.5px, 3847.5px, 0px)',}}>
+                  <ul className="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownRadioButton">
+                      <li>
+                          <div className="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                              <input checked id="filter-radio-example-3" type="radio" value="" name="filter-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+                              <label htmlFor="filter-radio-example-3" className="w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">All status</label>
+                          </div>
+                      </li>
+                  </ul>
               </div>
-              <input type="text" id="table-search" className="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for staff's name"></input>
+            </div>
+            <div className="absolute left-1/4 top-0">
+              <input type="text" id="table-search" className="block p-2 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for staff's name"></input>
+            </div>
+            <div className="absolute top-0 right-20">
+              <Button variant="outline" className="border-gray-300 bg-white hover:bg-slate-100"><Plus /></Button>
             </div>
           </div>
           <table className="border-collapse border border-black rounded-lg">
@@ -52,10 +49,7 @@ const StaffsPage = () => {
                 <th scope="col" className="px-6 py-3">
                   <div className="flex items-center">
                   Join date
-                    <a href="#">
-                    <svg className="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
-                    </svg></a>
+                    <a href="#"><ArrowUpDown className="w-3 h-3 ms-1.5" /></a>
                   </div>
                 </th>
                 <th scope="col" className="px-6 py-3">
@@ -73,7 +67,7 @@ const StaffsPage = () => {
               </tr>
             </thead>
             <tbody>
-              <tr className="odd:bg-white even:bg-gray-200 text-xs text-black">
+              <tr className="odd:bg-white even:bg-gray-100 text-xs text-black">
                 <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                   Nguyen Nguyen
                 </th>
@@ -96,8 +90,8 @@ const StaffsPage = () => {
                   Department name
                 </td>
                 <td className="px-6 py-4">
-                  <Button variant="outline" className="text-xs bg-white hover:bg-gray-200 border-blue-500 hover:border-blue-700 text-blue-500 hover:text-blue-700">View & edit</Button>
-                  <Button variant="outline" className="ml-2 text-xs bg-white hover:bg-gray-200 border-red-500 hover:border-red-700 text-red-500 hover:text-red-70">Delete</Button>
+                  <Button variant="outline" className="text-xs bg-white hover:bg-blue-500 border-blue-500 hover:border-blue-500 text-blue-500 hover:text-white">View & edit</Button>
+                  <Button variant="outline" className="ml-2 text-xs bg-white hover:bg-red-600 border-red-600 hover:border-red-600 text-red-600 hover:text-white">Delete</Button>
                 </td>
               </tr>
             </tbody>
